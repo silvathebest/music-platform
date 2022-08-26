91c355a33af0db938c4b3692d644676787b56d19
alexei <silverchat228@gmail.com>
2022-08-26 23:16:54 +0400

    init project

---
+++ src/main.ts
@@ -0,0 +1,15 @@
+import {NestFactory} from "@nestjs/core";
+import {AppModule} from "./app.module";
+
+const start = async () => {
+  try {
+    const PORT = process.env.PORT || 8080
+    const app = await NestFactory.create(AppModule)
+
+    await app.listen(PORT, () => console.log(`server started on PORT ${PORT}`))
+  } catch (e) {
+    console.error(e)
+  }
+}
+
+start()
